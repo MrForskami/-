d1dba2c9a03a7b15ac6df44c2547aec0524bc8db
Iepichnieyiighr <95309916+MrForskami@users.noreply.github.com>
2021-12-13 13:53:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+
+randdom = ["1", "2", "3", "4", "5", "6", "7", "8", "9","10"]
+
+randdom1 = randdom[randint(0, len(randdom)-1)]
+print(randdom1)
+
+print("")
+print("big problem")
+game = ["kill all", "world", "del tiktok", "del YouTube",]
+game1 = game[randint(0, len(game)-1)]
+
+print(game1)
